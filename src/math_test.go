b75a44d8fe6902a8bc838bd7fd09f5724aa0e1f7
redemption-001 <jmtakara02@gmail.com>
2022-06-06 14:18:00 -0300

    fix: :bug: clean

--- src/math_test.go
+++ src/math_test.go
@@ -1,12 +1,9 @@
 package main
 
 import "testing"
-import "fmt"
 
 func TestSoma(t *testing.T) {
-	total := Soma(11, 15)
-
-	t.Errorf(total)
+	total := Soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
